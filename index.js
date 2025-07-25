243172e1716800302209d7f67a74d319c9941baa
SRUTHIAGARWAL <bhagavathisruthiagarwal@gmail.com>
2025-07-25 22:28:37 +0530

    Error Handling using try-catch block

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express=require("express");
+const ExpressError=require("./ExpressError.js");
 const app=express();
 const mongoose=require("mongoose");
 const path=require("path");
 const Chat=require("./models/chats.js");
 const methodOverride = require('method-override'); // Import the module
+const { stat } = require("fs");
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
@@ -21,20 +23,27 @@ main().then(() => console.log("connection is succesfull")).catch(err => console.
 // });
 // chat1.save().then((res)=>{console.log(res);}).catch((err)=>console.log(err));
 //index route
-app.get("/chat",async (req,res)=>{
+app.get("/chat",async (req,res,next)=>{
+    try{
     let chats=await Chat.find({});
     // console.log(chats);
     res.render("index.ejs",{chats});
+    }
+    catch(err)
+    {
+        next(err);
+    }
 });
 //new post get request
 app.get("/chat/new", (req,res)=>
-    {
+    {   
         res.render("new.ejs");
     });
 //post request of new created post
-app.post("/chat",(req,res)=>
+app.post("/chat",async(req,res,next)=>
 {
-    let {from,msg,to}=req.body;
+    try{
+         let {from,msg,to}=req.body;
     let newChat=new Chat(
         {
             from:from,
@@ -42,38 +51,82 @@ app.post("/chat",(req,res)=>
             to:to,
             created_at: new Date()
         }
-    )
-    newChat.save()
-    .then((res)=>{console.log("chat saved successfully");})
-    .catch((err)=>console.log(err));
-    res.redirect("/chat");
+    );
+     await newChat.save()
+     res.redirect("/chat");
+    }
+    catch(err)
+    {
+        next(err);
+    }
 });
 //edit route
-app.get("/chat/:id/edit",async (req,res)=>{
-    let {id}=req.params;
+app.get("/chat/:id/edit",async (req,res,err)=>{
+   try{
+     let {id}=req.params;
     let chat= await Chat.findById(id);
     res.render("edit.ejs",{chat});
+   }
+   catch(err)
+   {
+    next(err);
+   }
 });
 // edit post route
-app.put("/chat/:id",async (req,res)=>{
-    let {id}=req.params;
+app.put("/chat/:id",async (req,res,next)=>{
+    try{
+        let {id}=req.params;
     let {msg: newmsg}=req.body;
     let updatedChat = await Chat.findByIdAndUpdate(id, {msg:newmsg},{runValidator : true , new: true});
     console.log(updatedChat);
     res.redirect("/chat");
+    }
+    catch(err)
+    {
+        next(err);
+    }
 });
 
 //delete route
-app.delete("/chat/:id/delete", async(req,res)=>{
+app.delete("/chat/:id/delete", async(req,res,next)=>{
+    try{
     let {id}=req.params;
     let chat = await Chat.findByIdAndDelete(id);
     res.redirect("/chat");
+    }
+    catch(err)
+    {
+        next(err);
+    }
 });
+
+//show route
+app.get("/chat/:id/show",async(req,res,next)=>{
+    try{
+     let {id}=req.params;
+    let chat= await Chat.findById(id);
+    if(!chat)
+    {
+        next(new ExpressError(404,"Chat not found"));
+    }
+    res.render("show.ejs",{chat});
+    }
+    catch(err)
+    {
+        next(err);
+    }
+})
 
 //root rounte
 app.get("/", (req,res)=>{
     res.send("route is working");
 });
+
+app.use((err,req,res,next)=>{
+    console.log("-----Error------");
+    let {status=500,message="something went wrong"}=err;
+    res.status(status).send(message);
+})
 app.listen(2901, ()=>
 {
     console.log("server is listening on port 2901");
